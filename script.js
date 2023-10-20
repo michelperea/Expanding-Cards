9cbbfdb737e691d3ab6531dfab6ff818c3d39569
Mike-369 <123855871+michelperea@users.noreply.github.com>
2023-10-20 01:16:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-// Selecciona todos los elementos con la clase 'panel'
-const panels = document.querySelectorAll('.panel');
+const panels = document.querySelectorAll('.panel')
 
-// Añade un evento de clic a cada panel
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
-        // Elimina las clases 'active' de todos los paneles
-        removeActiveClasses();
-        // Agrega la clase 'active' al panel clicado
-        panel.classList.add('active');
-    });
-});
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
 
-// Función para eliminar las clases 'active' de todos los paneles
 function removeActiveClasses() {
     panels.forEach(panel => {
-        panel.classList.remove('active');
-    });
+        panel.classList.remove('active')
+    })
 }
